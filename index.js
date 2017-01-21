f43e6302b9866bca7bfd702a994a0fe3f45df083
Shaural Patel <Shaural Patel>
2017-01-21 17:22:13 -0500

    add post and get calls to send and recieve data from node.js

--- index.js
+++ index.js
@@ -4,71 +4,34 @@ document.addEventListener('click', function() {
     e.preventDefault();
 
     chrome.runtime.sendMessage(document.getElementById("msg").value);
-    //var data = document.getElementById("msg").value;
-    //chrome.runtime.sendMessage(data);
 
-    // $.ajax({
-    //   type: "POST",
-    //   data: {data1: document.getElementById("msg").value},
-    //   dataType: 'json',
-    //   url: 'http://localhost:3000',
-    //   success: function(data) {
-    //     console.log("Success");
-    //   }
-    // });
+    // //Use XMLHTTPRequests for GET
+    var xhr = new XMLHttpRequest();
+    // xhr.open("GET", "http://LocalHost:3000/getdata", false);
+    // xhr.send();
+    // var result = xhr.responseText;
+    // chrome.runtime.sendMessage(result);
 
-    //chrome.runtime.sendMessage("test");
+    //Use XMLHTTPRequests for POST
+    // var http = new XMLHttpRequest();
+    // var url = "get_data.php";
+    // var params = {"id": "123", "name": "shaural",  "message": "this is a test message"};
+    // var j = JSON.stringify(params);
+    xhr.open("POST", "http://LocalHost:3000/storedata", false);
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.send(JSON.stringify({"id": "123", "name": "shaural",  "message": "this is a test message"}));
+
+    // http.onreadystatechange = function() {//Call a function when the state changes.
+    //     if(http.readyState == 4 && http.status == 200) {
+    //         alert(http.responseText);
+    //     }
+    // }
+    // xhr.send(params);
+
+
+    chrome.runtime.sendMessage(chrome.runtime.id); // This gets the extension unique id
+
 
   });
 
-  //alert("working");
-
-  //appendLog("sending to localhost:8888");
-  //chrome.runtime.sendMessage("working");
-    // {myMessage: text.value},
-    // function(response) {
-    //   appendLog("response: "+JSON.stringify(reponse));
-    // })
 });
-
-// chrome.runtime.sendMessage("hello");
-//
-// var main = document.getElementById("main");
-// var body = main.getElementById("body");
-// var form = body.getElementById("frm");
-// var text = form.getElementById("msg");
-// var send = body.getElementById("submit");
-//
-// chrome.runtime.sendMessage("test");
-//
-//
-// console.log(text.value);
-// console.log(form.value);
-//
-// function myalert(){
-//   alert("test");
-// }
-//
-// var helloWorld = function () {
-//   chrome.runtime.sendMessage('hI');
-// };
-//
-//
-// document.addEventListener('click', function() {
-//   body.getElementById("submit").addEventListener('click', function (e) {
-//     e.preventDefault();
-//     chrome.runtime.sendMessage('runtime message');
-//   });
-//
-//   //alert("working");
-//
-//   //appendLog("sending to localhost:8888");
-//   //chrome.runtime.sendMessage("working");
-//     // {myMessage: text.value},
-//     // function(response) {
-//     //   appendLog("response: "+JSON.stringify(reponse));
-//     // })
-// });
-
-
-// chrome.runtime.sendMessage('Hello World!');
