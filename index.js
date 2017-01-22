77044918f7da4a0e4af41d4610ebd8a36d99bdad
Lucas Tao <tao63@purdue.edu>
2017-01-21 20:08:10 -0500

    Fix duplicate strings appearing in chat (but chat does not stack)

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
+var temp = "";
 
 document.addEventListener('click', function() {
   document.getElementById("submit").addEventListener('click', function (e) {
     e.preventDefault();
 
-    chrome.runtime.sendMessage(document.getElementById("msg").value);
+    //chrome.runtime.sendMessage(document.getElementById("msg").value);
 
     // //Use XMLHTTPRequests for GET
     var xhr = new XMLHttpRequest();
-    // xhr.open("GET", "http://LocalHost:3000/getdata", false);
-    // xhr.send();
+
+    var pushChat = function() {
+      document.getElementById("ta").value = assign();
+    }
+
+    var deletePrevious = function() {
+        while (result.includes("||")) {
+          var loc = result.indexOf("||");
+          result = result.substring(loc+1, result.length);
+        }
+    }
+
+    var assign = function() {
+      deletePrevious();
+      //temp += result;
+      //result = temp;
+      //temp = temp.replace(result, '');
+      //temp += result;
+      return result;
+    }
+
+
     // var result = xhr.responseText;
-    // chrome.runtime.sendMessage(result);
+
+
+
+
+
+
 
     //Use XMLHTTPRequests for POST
     // var http = new XMLHttpRequest();
@@ -19,7 +45,21 @@ document.addEventListener('click', function() {
     // var j = JSON.stringify(params);
     xhr.open("POST", "http://LocalHost:3000/storedata", false);
     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhr.send(JSON.stringify({"id": "123", "name": "shaural",  "message": "this is a test message"}));
+    xhr.send(JSON.stringify({"id": "123", "name": "shaural",  "message": document.getElementById("msg").value}));
+
+
+    //var result = xhr.responseText;
+
+
+    xhr.open("GET", "http://LocalHost:3000/getdata", false);
+    xhr.send();
+    var result = xhr.responseText;
+
+    //chrome.runtime.sendMessage(result);
+    // function checkFile() {
+    //   val = setInterval(pushChat, 200);
+    // }
+    pushChat();
 
     // http.onreadystatechange = function() {//Call a function when the state changes.
     //     if(http.readyState == 4 && http.status == 200) {
@@ -29,7 +69,7 @@ document.addEventListener('click', function() {
     // xhr.send(params);
 
 
-    chrome.runtime.sendMessage(chrome.runtime.id); // This gets the extension unique id
+    //chrome.runtime.sendMessage(chrome.runtime.id); // This gets the extension unique id
 
 
   });
