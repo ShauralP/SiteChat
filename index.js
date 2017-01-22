a0733fe45b947b0518584ea44b069f7ffc154b49
Lucas Tao <tao63@purdue.edu>
2017-01-21 22:15:08 -0500

    Fix chat, line by line styled input

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var temp = "";
 var ip = "";
 var time = "";
 var message = "";
+// var array = [""];
 var messageCounter = 0;
 
 document.addEventListener('click', function() {
@@ -14,11 +15,20 @@ document.addEventListener('click', function() {
     var xhr = new XMLHttpRequest();
 
     var pushChat = function() {
-      //cleanUp();
-      //messageCounter++;
+      cleanUp();
+      messageCounter++;
       //document.getElementById("ta").value = assign();
       //temp += message;
-      document.getElementById("ta").value = result;
+      // array.push(message);
+      // for (int i=0; i < array.length; i++) {
+      //   temp += array[i];
+      // }
+      if (temp.indexOf(message) > -1) {
+
+      } else {
+        temp += message + "\n";
+      }
+      document.getElementById("ta").value = temp;
       //document.getElementById("ta").value = message;
     }
 
@@ -30,23 +40,25 @@ document.addEventListener('click', function() {
     }
 
     var cleanUp = function() {
-      var counter = 0;
-      while (result.includes("~")) {
-        var loc1 = result.indexOf("~");
-        var loc2 = result.indexOf("~", loc1+1);
-        if (counter == messageCounter*4 + 0) {
-          ip = result.substring(loc1+1, loc2);
-        } else if (counter == messageCounter*4 + 1) {
-          time = result.substring(loc1+1, loc2);
-        } else if (counter == messageCounter*4 + 2) {
-          message = result.substring(loc1+1, loc2) + ": ";
-        } else if (counter == messageCounter*4 + 3) {
-          message += result.substring(loc1+1, loc2);
-          break;
-        }
-        result = result.substring(loc2, result.length);
-        counter++;
-      }
+      deletePrevious();
+
+     var counter = 0;
+     while (result.includes("~")) {
+       var loc1 = result.indexOf("~");
+       var loc2 = result.indexOf("~", loc1+1);
+       if (counter ==  0) {
+         ip = result.substring(loc1+1, loc2);
+       } else if (counter == 1) {
+         time = result.substring(loc1+1, loc2);
+       } else if (counter ==  2) {
+         message = result.substring(loc1+1, loc2) + ": ";
+       } else if (counter ==  3) {
+         message += result.substring(loc1+1, loc2);
+         break;
+       }
+       result = result.substring(loc2, result.length);
+       counter++;
+     }
     }
 
     var assign = function() {
@@ -102,9 +114,9 @@ document.addEventListener('click', function() {
 
     // }
     pushChat();
-    
 
-    
+
+
     // http.onreadystatechange = function() {//Call a function when the state changes.
     //     if(http.readyState == 4 && http.status == 200) {
     //         alert(http.responseText);
